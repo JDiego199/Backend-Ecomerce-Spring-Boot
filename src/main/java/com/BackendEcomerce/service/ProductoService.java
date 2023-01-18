083b0ed1c1d67400861d9fc6f25a83635ddeb8b6
durazno641 <87629354+durazno641@users.noreply.github.com>
2023-01-18 09:57:17 -0500

    Update ProductoService.java

--- src/main/java/com/BackendEcomerce/service/ProductoService.java
+++ src/main/java/com/BackendEcomerce/service/ProductoService.java
@@ -12,19 +12,23 @@ import java.util.List;
  * @author mota1
  */
 public interface ProductoService {
-   /*  List<Producto> getAll();
+
+    /*  List<Producto> getAll();
       Producto getById(int id);*/
 
-   public List<Producto> findAll();
+    public List<Producto> findAll();
 
-   public Producto save(Producto producto);
+    public Producto save(Producto producto);
 
-   public Producto findById(Integer id);
+    public Producto findById(Integer id);
+    public Producto findByIdMay0(Integer id);
 
-   public void delete(Integer id);
-   
-  public boolean validarStoc(Integer catindad, Integer id);
-  
+    public void delete(Integer id);
+
+    public boolean validarStoc(Integer catindad, Integer id);
+
     public List<Producto> findAllEmpresa(Integer id);
+
+    public boolean ControlNumeroProduct(Integer id);
 
 }
