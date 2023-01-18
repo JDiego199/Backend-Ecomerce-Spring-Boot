45974139a59cec3f8ddf2eaf55c165336a0d923e
durazno641 <87629354+durazno641@users.noreply.github.com>
2023-01-18 10:01:17 -0500

    Update FeedbackServiceImplement.java

--- src/main/java/com/BackendEcomerce/service/FeedbackServiceImplement.java
+++ src/main/java/com/BackendEcomerce/service/FeedbackServiceImplement.java
@@ -6,6 +6,7 @@ package com.BackendEcomerce.service;
 
 import com.BackendEcomerce.Repository.FeedbackRepository;
 import com.BackendEcomerce.model.Feedback;
+import com.BackendEcomerce.model.Ordenes;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.BackendEcomerce.Repository.OrdenesRepository;
 
 /**
  * @author mota1
@@ -23,6 +25,8 @@ public class FeedbackServiceImplement implements FeedbackService {
 
    @Autowired
    private FeedbackRepository feedbackRepository;
+       @Autowired
+    private OrdenesRepository ordenesRepository;
 
 
    @Override
@@ -50,4 +54,17 @@ public class FeedbackServiceImplement implements FeedbackService {
    public void delete(Integer id) {
       feedbackRepository.deleteById(id);
    }
+
+    @Override
+    public boolean PermisoComentarFeedback(Integer id, Integer idEmpresa, Feedback feedback) {
+        Iterable<Ordenes> orders = ordenesRepository.findAll();
+        for (Ordenes order : orders) {
+            if ((order.getCliente_persona().getId_persona().equals(id)) && (order.getOrdenes_detalles().getProducto().getCliente_empresa().getId_empresa().equals(idEmpresa))) {              
+                  feedbackRepository.save(feedback);
+                  return true;
+            }
+        }
+        return false;
+    }
+
 }
