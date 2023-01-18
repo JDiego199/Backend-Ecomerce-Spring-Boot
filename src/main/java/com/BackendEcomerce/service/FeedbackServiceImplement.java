ab1db4750d7bd8ad411f0427d036777d560b26ff
durazno641 <87629354+durazno641@users.noreply.github.com>
2023-01-18 08:57:00 -0500

    Update FeedbackServiceImplement.java

--- src/main/java/com/BackendEcomerce/service/FeedbackServiceImplement.java
+++ src/main/java/com/BackendEcomerce/service/FeedbackServiceImplement.java
@@ -4,8 +4,16 @@
  */
 package com.BackendEcomerce.service;
 
+import com.BackendEcomerce.Repository.ClienteRepository;
+import com.BackendEcomerce.Repository.Cliente_empresaRepository;
+import com.BackendEcomerce.model.Cliente_empresa;
 import com.BackendEcomerce.Repository.FeedbackRepository;
+import com.BackendEcomerce.model.Cliente;
 import com.BackendEcomerce.model.Feedback;
+import com.BackendEcomerce.model.Producto;
+import com.BackendEcomerce.Repository.Cliente_personaRepository;
+import com.BackendEcomerce.Repository.OrdenesRepository;
+import com.BackendEcomerce.model.Ordenes;
 
 import java.util.List;
 
@@ -21,33 +29,47 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class FeedbackServiceImplement implements FeedbackService {
 
-   @Autowired
-   private FeedbackRepository feedbackRepository;
+    @Autowired
+    private FeedbackRepository feedbackRepository;
+    @Autowired
+    private OrdenesRepository ordenesRepository;
 
+    @Override
+    @Transactional(readOnly = true)
+    public List<Feedback> findAll() {
+        return (List<Feedback>) feedbackRepository.findAll();
 
-   @Override
-   @Transactional(readOnly = true)
-   public List<Feedback> findAll() {
-      return (List<Feedback>) feedbackRepository.findAll();
+    }
 
-   }
+    @Override
+    @Transactional(readOnly = false)
+    public Feedback save(Feedback feedback) {
+        
+        return feedbackRepository.save(feedback);
+    }
 
-   @Override
-   @Transactional(readOnly = false)
-   public Feedback save(Feedback feedback) {
+    @Override
+    @Transactional(readOnly = true)
+    public Feedback findById(Integer id) {
+        return feedbackRepository.findById(id).orElse(null);
+    }
 
-      return feedbackRepository.save(feedback);
-   }
+    @Override
+    @Transactional(readOnly = false)
+    public void delete(Integer id) {
+        feedbackRepository.deleteById(id);
+    }
 
-   @Override
-   @Transactional(readOnly = true)
-   public Feedback findById(Integer id) {
-      return feedbackRepository.findById(id).orElse(null);
-   }
+    @Override
+    public boolean PermisoComentarFeedback(Integer id, Integer idEmpresa, Feedback feedback) {
+        Iterable<Ordenes> orders = ordenesRepository.findAll();
+        for (Ordenes order : orders) {
+            if ((order.getCliente_persona().getId_persona().equals(id)) && (order.getOrdenes_detalles().getProducto().getCliente_empresa().getId_empresa().equals(idEmpresa))) {              
+                  feedbackRepository.save(feedback);
+                  return true;
+            }
+        }
+        return false;
+    }
 
-   @Override
-   @Transactional(readOnly = false)
-   public void delete(Integer id) {
-      feedbackRepository.deleteById(id);
-   }
 }
